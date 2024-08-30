15e4c7280ced185233a359e79aa944b1dd123bb3
Oliver Pan <2216991777@qq.com>
2024-08-29 23:29:00 -0500

    chore:added new contents

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,96 @@ import {
 } from "@tabler/icons-react";
 import Link from "next/link";
 
-import Window from "@/components/core/window";
-import { Ornament, OrnamentItem } from "@/components/core/ornament";
+import { Window, WindowControls } from "@/components/core/window";
+import {
+  OrnamentTabs,
+  OrnamentContent,
+  OrnamentTrigger,
+  Ornament,
+  OrnamentContents,
+} from "@/components/core/ornament";
 import { SVGProps } from "react";
+
+import bg from "@/public/assets/background-night.png";
+import Image from "next/image";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import { Text } from "@/components/ui/typography";
 
 export default function HomePage() {
   return (
     <>
       <Cursor />
-      <main
-        className="flex min-h-screen flex-col justify-center space-y-8 text-center"
-        data-vision-pro-ui
-      >
-        <h1 className="text-2xl font-bold">VisionOS UI</h1>
+      <main className="flex min-h-screen max-w-none flex-col justify-center space-y-8 text-center">
+        <AspectRatio
+          ratio={2610 / 1468}
+          className="isolate flex max-h-[1468px] max-w-[2610px] items-center justify-center"
+          data-vision-os-ui
+        >
+          <Ornament defaultTab="memories">
+            <div className="mb-[18.5px]">
+              <OrnamentTabs>
+                <OrnamentTrigger
+                  icon={<IconHistory />}
+                  label="Memories"
+                  value="memories"
+                />
+                <OrnamentTrigger
+                  icon={<IconPhotoFilled />}
+                  label="Library"
+                  value="library"
+                />
+                <OrnamentTrigger
+                  icon={<IconPanoramaHorizontalFilled />}
+                  label="Panorama"
+                  value="panorama"
+                />
+                {/* <OrnamentItem
+                  icon={<StackIcon className="size-6" />}
+                  label="Albums"
+                  value="albums"
+                />
+                <OrnamentItem
+                  icon={<IconSearch />}
+                  label="Search"
+                  value="search"
+                /> */}
+              </OrnamentTabs>
+            </div>
+            <div className="grid h-[max(520px,50%)] max-h-[max(520px,50%)] w-full grid-rows-[1fr_37px] place-items-center">
+              <OrnamentContents>
+                <OrnamentContent value="memories" key="memories">
+                  <div className="flex h-[92px] items-center justify-center">
+                    <Text variant="default" asChild>
+                      <h1 className="text-xl font-semibold">VisionOS UI</h1>
+                    </Text>
+                  </div>
+                </OrnamentContent>
+                <OrnamentContent value="library" key="library">
+                  <div className="flex h-[92px] items-center justify-center">
+                    <Text variant="default" asChild>
+                      <h1 className="text-xl font-semibold">VisionOS UI 2</h1>
+                    </Text>
+                  </div>
+                </OrnamentContent>
+                <OrnamentContent value="panorama" key="panorama">
+                  <div className="flex h-[92px] items-center justify-center">
+                    <Text variant="default" asChild>
+                      <h1 className="text-xl font-semibold">VisionOS UI 3</h1>
+                    </Text>
+                  </div>
+                </OrnamentContent>
+              </OrnamentContents>
+              <WindowControls />
+            </div>
+          </Ornament>
+          <Image
+            src={bg}
+            alt="background"
+            unoptimized
+            className="absolute inset-0 z-[-1] rounded-3xl p-1.5"
+          />
+        </AspectRatio>
+
         <p className="text-fd-muted-foreground">
           You can open{" "}
           <Link
@@ -51,7 +128,7 @@ export default function HomePage() {
           <Button className="rounded-full" size="icon">
             <Ellipsis data-slot="icon" />
           </Button>
-          <Button className="rounded-full" size="icon">
+          <Button className="rounded-full" size="icon" disabled>
             <PenBox data-slot="icon" />
           </Button>
         </Window>
@@ -74,32 +151,6 @@ export default function HomePage() {
                 <IconSearch data-slot="icon" />
               </Button>
             </Window>
-          </div>
-        </div>
-        <div className="flex items-center justify-center">
-          <div className="flex min-w-[20rem] items-center justify-center rounded-md bg-gradient-to-tr from-rose-700/40 via-indigo-700/40 to-cyan-700/40 p-8">
-            <Ornament>
-              <OrnamentItem
-                icon={<IconHistory data-slot="icon" />}
-                label="Memories"
-              />
-              <OrnamentItem
-                icon={<IconPhotoFilled data-slot="icon" />}
-                label="Library"
-              />
-              <OrnamentItem
-                icon={<IconPanoramaHorizontalFilled data-slot="icon" />}
-                label="Panorama"
-              />
-              <OrnamentItem
-                icon={<StackIcon className="size-6" data-slot="icon" />}
-                label="Albums"
-              />
-              <OrnamentItem
-                icon={<IconSearch data-slot="icon" />}
-                label="Search"
-              />
-            </Ornament>
           </div>
         </div>
       </main>
