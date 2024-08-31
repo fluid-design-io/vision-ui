535d7995d75457bf0f02891f69d9f900711ff988
Oliver Pan <2216991777@qq.com>
2024-08-31 00:16:12 -0500

    feat: add typography

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -3,20 +3,23 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, VariantProps } from "class-variance-authority";
 import React from "react";
 
+const baseStyle = "transition-all duration-500";
+
 export const textStyles = {
   default: {
-    top: "text-[rgb(84,84,84)] opacity-0",
-    bottom: "text-white opacity-95 transition-all duration-300",
+    bottom: cn(baseStyle, "text-[#545454] opacity-0 mix-blend-color-dodge"),
+    top: cn(baseStyle, "text-white opacity-95"),
   },
   secondary: {
-    top: "text-[rgb(84,84,84)] mix-blend-color-dodge opacity-100",
-    bottom:
-      "text-white mix-blend-lighten opacity-50 transition-all duration-300",
+    bottom: cn(baseStyle, "text-[#545454] mix-blend-color-dodge opacity-100"),
+    top: cn(baseStyle, "text-white opacity-65 mix-blend-lighten"),
   },
   tertiary: {
-    top: "text-[rgb(94,94,94)] mix-blend-color-dodge opacity-20",
-    bottom:
-      "text-white mix-blend-lighten opacity-10 transition-all duration-300",
+    bottom: cn(
+      baseStyle,
+      "text-[#5E5E5E] mix-blend-color-dodge opacity-[0.21]",
+    ),
+    top: cn(baseStyle, "text-white opacity-[0.31]"),
   },
 };
 
@@ -27,9 +30,25 @@ export const textVariants = cva("", {
       secondary: "",
       tertiary: "",
     },
+    size: {
+      title1: "leading-loose text-2xl font-bold",
+      title2: "leading-7 text-[22px] font-bold",
+      title3: "leading-normal text-[19px] font-bold",
+      largeTitle: "leading-[38px]text-[29px] font-bold",
+      XLTitle1: "leading-[56px] text-5xl font-bold",
+      XLTitle2: "leading-[46px] text-[38px] font-bold",
+      headline: "leading-snug text-[17px] font-bold",
+      body: "leading-snug text-[17px]",
+      callout: "leading-tight text-[15px]",
+      subheadline: "leading-tight text-[15px] font-normal",
+      footnote: "leading-[18px] text-[13px]",
+      caption1: "leading-none text-xs",
+      caption2: "leading-none text-xs",
+    },
   },
   defaultVariants: {
     variant: "default",
+    size: "body",
   },
 });
 
@@ -40,15 +59,15 @@ export interface TextProps
 }
 
 const Text = React.forwardRef<HTMLParagraphElement, TextProps>(
-  ({ className, variant, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "p";
 
     return (
       <div className="relative">
         <Comp
           className={cn(
-            "absolute inset-0 z-[-1]",
-            textVariants({ variant, className }),
+            "absolute inset-0",
+            textVariants({ variant, size, className }),
             textStyles[variant ?? "default"].bottom,
           )}
           ref={ref}
@@ -56,7 +75,7 @@ const Text = React.forwardRef<HTMLParagraphElement, TextProps>(
         />
         <Comp
           className={cn(
-            textVariants({ variant, className }),
+            textVariants({ variant, size, className }),
             textStyles[variant ?? "default"].top,
           )}
           ref={ref}
